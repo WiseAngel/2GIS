cf78051c09c67419b893f58e6ff0a6050da84c8b
WiseAngel <wiseangelgames@gmail.com>
2021-07-28 14:43:07 +0300

    init

---
+++ tests/test_ps.py
@@ -0,0 +1,20 @@
+from requests import request
+
+endpoint = 'https://regions-test.2gis.com/1.0/regions'
+
+
+def test_ps():
+    print(endpoint)
+    url = f'{endpoint}?page_size='
+    ps = [5, 10, 15]
+    payload = {}
+    headers = {}
+    for i in ps:
+
+        response = request(
+            "GET", url+str(i), headers=headers, data=payload)
+        res = response.json()
+
+        assert res['total'] == 22, 'total'
+        assert response.status_code == 200, 'status code'
+        assert len(res['items']) == i, 'len'
