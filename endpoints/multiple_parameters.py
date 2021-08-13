0754f89882279b133a8064d266a669a7a9e7c24d
WiseAngel <wiseangelgames@gmail.com>
2021-08-13 17:25:34 +0300

    Изменяется список значений переданный в multiple_parametrs DataGenerator

--- endpoints/multiple_parameters.py
+++ endpoints/multiple_parameters.py
@@ -1,30 +1,42 @@
 class DataGenerator():
+    def __init__(self, *value):
+        self.max_value_len = 0
+        self.value = value
+        self.list_of_lists_by_number_of_endpoints = [] 
 
-    max_value_len: int
-
-    def get_max_len(self, value):
-        self.max_value_len = len(max(value, key=len))
+    def get_max_len(self,value):
+        self.max_value_len = len(max(self.value, key=len))
         return self.max_value_len
 
     def fill_list(self, num):
-        list_of_lists_by_number_of_endpoints = [[] for i in range(num)]
-        return list_of_lists_by_number_of_endpoints
+        self.list_of_lists_by_number_of_endpoints = [[] for _ in range(self.max_value_len)]
+        return self.list_of_lists_by_number_of_endpoints
+
+    # def equalize_length_of_lists(self, value):
+    #     sorted_list = sorted(self.value, key=len)
+    #     while not len(sorted_list[0]) == self.max_value_len:
+    #         sorted_list = sorted(self.value, key=len)
+    #         for v in self.value:
+    #             if len(v) < self.max_value_len:
+    #                 diff = self.max_value_len - len(v)
+    #                 v += v[:diff]
+    #     return(self.value)
 
     def equalize_length_of_lists(self, value):
-        sorted_list = sorted(value, key=len)
-        while not len(sorted_list[0]) == self.max_value_len:
-            sorted_list = sorted(value, key=len)
-            for v in value:
-                if len(v) < self.max_value_len:
-                    diff = self.max_value_len - len(v)
-                    v += v[:diff]
+        value  = self.value
+        for v in value:
+            if len(v) < self.max_value_len:
+                diff = self.max_value_len - len(v)
+                v += v[:diff]
         return(value)
 
-    def bar(self, *value):
-        max_len = self.get_max_len(value)
-        list_by_number_of_endpoints = self.fill_list(max_len)
-        equalized_lists_data  = self.equalize_length_of_lists(value)
+    def bar(self):
+        print(self.value)
+        self.get_max_len(self.value)
+        self.fill_list(self.max_value_len)
+        equalized_lists_data  = self.equalize_length_of_lists(self.value)
+        print(equalized_lists_data)
         for e in equalized_lists_data :
             for i, v in enumerate(e):
-                list_by_number_of_endpoints[i].append(v)
-        return (list_by_number_of_endpoints)
+                self.list_of_lists_by_number_of_endpoints[i].append(v)
+        return (self.list_of_lists_by_number_of_endpoints)
--- test_multiple_parameters.py
+++ test_multiple_parameters.py
@@ -4,19 +4,29 @@ from endpoints import *
 
 
 sr = SendRequest()
-dg = DataGenerator()
-v1 = dg.bar(page_size_positive_values, page_positive_values, q_positive_values )
-v = dg.bar(page_size_negative_values, page_negative_values)
+dg = DataGenerator(page_size_positive_values, page_positive_values, q_positive_values)
+v1 = dg.bar()
+print()
+print()
+dg2 = DataGenerator(page_size_negative_values, page_negative_values)
+v = dg2.bar()
 
-@pytest.mark.parametrize('endpoint', [['page_size','page']])
-@pytest.mark.parametrize('value', [*v])
-def test_status_code_is_200(endpoint, value):
-    response = sr.send_get_request_with_multiple_parameters(endpoint, value)
-    status_code_is_200(response, value)
 
-# Parameter total is equal to the total number of regions
-@pytest.mark.parametrize('endpoint', [['page_size','page', 'q']])
-@pytest.mark.parametrize('value', [*v1])
-def test_total_count(endpoint, value):
-    response = sr.send_get_request_with_multiple_parameters(endpoint, value).json() 
-    check_total(response)
+# @pytest.mark.parametrize('endpoint', [['page_size','page']])
+# @pytest.mark.parametrize('value', [*v])
+# def test_status_code_is_200(endpoint, value):
+#     response = sr.send_get_request_with_multiple_parameters(endpoint, value)
+#     status_code_is_200(response, value)
+
+# # Parameter total is equal to the total number of regions
+# @pytest.mark.parametrize('endpoint', [['page_size','page', 'q']])
+# @pytest.mark.parametrize('value', [*v1])
+# def test_total_count(endpoint, value):
+#     response = sr.send_get_request_with_multiple_parameters(endpoint, value).json() 
+#     check_total(response)
+import time
+time.sleep(5)
+print()
+print()
+dg3 = DataGenerator(page_size_positive_values, country_code_positive_values)
+v2 = dg3.bar()
